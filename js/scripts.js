2db790934c5fc00487be57bc45ccc6c5ef4709f7
Matt Rosanio and Neil Larion <mmrosani@syr.edu>
2016-01-25 10:37:52 -0800

    add jquery functionallity to add and remove list items

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,28 @@ function ToDo(task,note){
   this.task = task;
   this.note = note;
 }
-
+var list = 0;
 $(function(){
   $("form#newToDo").submit(function(event){
+    list++;
     event.preventDefault();
     var task = $("#task").val();
     var note = $("#note").val();
     var newToDo = new ToDo(task,note);
-    $("#tasks").append("<li><span class='toDo'>"+newToDo.task+"</span></li>");
+    $("#tasks").append("<li class='toDo' id='task"+list+"'>"+newToDo.task+"</li>");
 
     $("input#task").val("");
     $("input#note").val("");
 
     $('.toDo').last().click(function() {
+      var someId = $(this).attr("id");
       $("#show-task").show();
       $("#show-task h2").text(newToDo.task);
       $(".note").text(newToDo.note);
+      $(".buttonContainer").text("Remove").click(function(){
+        $("#show-task").hide();
+        $('#'+someId).hide();
+      });
     });
   });
-
 });
